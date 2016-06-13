9ff64eeb3a6491f3cfd1659fc80ea0009d524f3a
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-13 17:13:39 -0400

    funcionalidad de menú y cierre de sesión (aún sin poder visualizarlo)

--- app/src/main/java/edu/galileo/android/twitterclient/main/ui/MainActivity.java
+++ app/src/main/java/edu/galileo/android/twitterclient/main/ui/MainActivity.java
@@ -1,8 +1,14 @@
 package edu.galileo.android.twitterclient.main.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.twitter.sdk.android.Twitter;
+
+import edu.galileo.android.twitterclient.LoginActivity;
 import edu.galileo.android.twitterclient.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        Twitter.logOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
