c079f3738c2ed76abe8bf9a2a04669fb3d42e4ec
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-13 16:32:39 -0400

    se utiliza un botón especial para el login de twitter TwitterLoginButton en el layout y en la actividad se realiza inyección de vistas con butterKnife para luego especificar al botón un callback

--- app/src/main/java/edu/galileo/android/twitterclient/LoginActivity.java
+++ app/src/main/java/edu/galileo/android/twitterclient/LoginActivity.java
@@ -1,13 +1,44 @@
 package edu.galileo.android.twitterclient;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.twitterLoginButton)
+    TwitterLoginButton twitterLoginButton;
+    @Bind(R.id.container)
+    RelativeLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                navigateToMainScreen();
+            }
+
+            @Override
+            public void failure(TwitterException e) {
+
+            }
+        });
+    }
+
+    private void navigateToMainScreen() {
     }
 }
