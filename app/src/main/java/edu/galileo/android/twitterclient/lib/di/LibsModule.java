7df0801047a4114bb9729ef1b6b1bd55adc77806
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-13 16:00:01 -0400

    se crea un nuevo paquete "di" de Dependency Injection. Una clase LibsModule que define ciertas anotaciones de dagger2 para las inyecciones. @Module que indica que la clase es un módulo @Singleton indica que solamente habrá una única instancia para toda la app @Provides indica que provee la inyección de dependencia
    Aquí, se proveerá el EventBus en base al parámetro recibido en el constructor definido en GreenRobotEventBus

---
+++ app/src/main/java/edu/galileo/android/twitterclient/lib/di/LibsModule.java
@@ -0,0 +1,21 @@
+package edu.galileo.android.twitterclient.lib.di;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import edu.galileo.android.twitterclient.lib.GreenRobotEventBus;
+import edu.galileo.android.twitterclient.lib.base.EventBus;
+
+/**
+ * Created by carlos.gomez on 13/06/2016.
+ */
+@Module
+public class LibsModule {
+
+    @Provides
+    @Singleton
+    EventBus providesEventBus(org.greenrobot.eventbus.EventBus eventBus){
+        return new GreenRobotEventBus(eventBus);
+    }
+}
