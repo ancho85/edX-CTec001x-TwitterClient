6a5358969b7cd3f09cae10fc79fea3111f010fe6
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-13 16:46:44 -0400

    manejo de mensaje de error con snackbar y reutilización de sesión abierta

--- app/src/main/java/edu/galileo/android/twitterclient/LoginActivity.java
+++ app/src/main/java/edu/galileo/android/twitterclient/LoginActivity.java
@@ -2,9 +2,11 @@ package edu.galileo.android.twitterclient;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RelativeLayout;
 
+import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
@@ -28,6 +30,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        if (Twitter.getSessionManager().getActiveSession() != null){
+            navigateToMainScreen();
+        }
+
         twitterLoginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
@@ -36,7 +42,9 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void failure(TwitterException e) {
-
+                String msgError = String.format(getString(R.string.login_error_message),
+                        e.getLocalizedMessage());
+                Snackbar.make(container, msgError, Snackbar.LENGTH_SHORT).show();
             }
         });
     }
