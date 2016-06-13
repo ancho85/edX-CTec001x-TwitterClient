2b228bd94a58ff3fa20d09b4879f8dcb54eb59e2
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-13 15:09:25 -0400

    el plugin de Fabric para AndroidStudio modifica LoginActivity para incluir la autenticación y la conexión a twitter utilizando los tokens en forma de valores constantes. Se crea una clase que hereda de Application y se define allí esta configuración, tomando los valores desde la configuración de gradle

---
+++ app/src/main/java/edu/galileo/android/twitterclient/TwitterClientApp.java
@@ -0,0 +1,24 @@
+package edu.galileo.android.twitterclient;
+
+import android.app.Application;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by carlos.gomez on 13/06/2016.
+ */
+public class TwitterClientApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initFabric();
+    }
+
+    private void initFabric() {
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+}
