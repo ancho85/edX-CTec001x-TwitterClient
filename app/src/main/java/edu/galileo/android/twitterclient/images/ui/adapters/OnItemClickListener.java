4203c00fffb2664f7df428aeba4386458678c7b3
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-13 20:18:26 -0400

    adaptador para Images

---
+++ app/src/main/java/edu/galileo/android/twitterclient/images/ui/adapters/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package edu.galileo.android.twitterclient.images.ui.adapters;
+
+import edu.galileo.android.twitterclient.entities.Image;
+
+/**
+ * Created by carlos.gomez on 13/06/2016.
+ */
+public interface OnItemClickListener {
+    void onItemClick(Image image);
+}
