5bad297eb6f930a014d9a0f1ee04501ea0b9faf0
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-14 17:33:54 -0400

    bugfix en el layout utilizado para mostrar los hashtags

--- app/src/main/java/edu/galileo/android/twitterclient/hashtags/ui/adapters/HashtagsAdapter.java
+++ app/src/main/java/edu/galileo/android/twitterclient/hashtags/ui/adapters/HashtagsAdapter.java
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,7 +29,7 @@ public class HashtagsAdapter extends RecyclerView.Adapter<HashtagsAdapter.ViewHo
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_images, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_hashtags, parent, false);
         return new ViewHolder(view, parent.getContext());
     }
 
