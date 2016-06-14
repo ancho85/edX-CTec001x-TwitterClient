50a95326b32fbb10d89aedf76f0f9591532b5586
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-14 09:54:33 -0400

    definidos los provides para el target, en este caso el presentador para el fragment

--- app/src/main/java/edu/galileo/android/twitterclient/images/di/ImagesModule.java
+++ app/src/main/java/edu/galileo/android/twitterclient/images/di/ImagesModule.java
@@ -8,9 +8,15 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import edu.galileo.android.twitterclient.entities.Image;
+import edu.galileo.android.twitterclient.images.ImagesInteractor;
+import edu.galileo.android.twitterclient.images.ImagesInteractorImpl;
+import edu.galileo.android.twitterclient.images.ImagesPresenter;
+import edu.galileo.android.twitterclient.images.ImagesPresenterImpl;
+import edu.galileo.android.twitterclient.images.ImagesRepository;
 import edu.galileo.android.twitterclient.images.ui.ImagesView;
 import edu.galileo.android.twitterclient.images.ui.adapters.ImagesAdapter;
 import edu.galileo.android.twitterclient.images.ui.adapters.OnItemClickListener;
+import edu.galileo.android.twitterclient.lib.base.EventBus;
 import edu.galileo.android.twitterclient.lib.base.ImageLoader;
 
 /**
@@ -35,21 +41,47 @@ public class ImagesModule {
     //      permitir acceder al imageLoader
     @Provides
     @Singleton
-    ImagesAdapter providesAdapter(List<Image> items, ImageLoader imageLoader, OnItemClickListener clickListener){
+    ImagesAdapter providesAdapter(List<Image> items, ImageLoader imageLoader, OnItemClickListener clickListener) {
         return new ImagesAdapter(items, imageLoader, clickListener);
     }
 
     //Devolver el listener definido en el constructor
     @Provides
     @Singleton
-    OnItemClickListener providesOnItemClickListener(){
+    OnItemClickListener providesOnItemClickListener() {
         return this.clickListener;
     }
 
     //Devolver el listado de imágenes
     @Provides
     @Singleton
-    List<Image> providesItemsList(){
+    List<Image> providesItemsList() {
         return new ArrayList<Image>();
     }
+
+    //El objeto que se va a tener dentro del target, en este caso es el Fragment, además del adapter
+    // ya definido arriba, es el Presentador, entonces hay que proveerlo también
+    //OBS:  eventBus ya tiene definido un @Provides en LibModule, por el mismo motivo ya comentado. Se definen los otros
+    //      Los parámetros que recive este provider también se toman desde el constructor de lo que se va a devolver
+    @Provides
+    @Singleton
+    ImagesPresenter providesImagesPresenter(ImagesView view, EventBus eventBus, ImagesInteractor interactor) {
+        return new ImagesPresenterImpl(view, eventBus, interactor);
+    }
+
+    //devolver el imageView desde el constructor
+    @Provides
+    @Singleton
+    ImagesView providesImagesView() {
+        return this.view;
+    }
+
+    //devolver un interactor desde la implementación de la interface
+    @Provides
+    @Singleton
+    ImagesInteractor providesImagesInteractor(ImagesRepository repository) {
+        return new ImagesInteractorImpl(repository);
+    }
+
+    
 }
